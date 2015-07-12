9ac32dd4f53c1e3b24f8e9feb40b8131aef7a747
Uli Kusterer <ulivc@zathras.de>
2015-07-12 18:05:11 +0200

    Initial Commit

---
+++ WebKammerTests/WebKammerTests.swift
@@ -0,0 +1,36 @@
+//
+//  WebKammerTests.swift
+//  WebKammerTests
+//
+//  Created by Uli Kusterer on 12/07/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class WebKammerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
